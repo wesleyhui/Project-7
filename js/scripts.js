0ffe780ea556b9e4ca489a361d79c6aea40185eb
Wesley Hui <wesleyhui21@gmail.com>
2020-05-27 18:26:06 -0700

    update css + js

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+/*----Variables----*/
+const alertBanner = document.getElementById("alert");
+const trafficCanvas = document.getElementById("traffic_chart");
+const dailyCanvas = document.getElementById("daily_chart");
+const mobileCanvas = document.getElementById("mobile_chart");
+const user = document.getElementById("userField");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
+
+/*----Alert Banners----*/
