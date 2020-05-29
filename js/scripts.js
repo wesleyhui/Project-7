59240d50efd130cf7b969ed26092736554d08d41
Wesley Hui <wesleyhui21@gmail.com>
2020-05-29 10:20:16 -0700

    finish script

--- js/scripts.js
+++ js/scripts.js
@@ -125,3 +125,34 @@ const mobileData = {
     }]
 }
 
+const mobileOptions = {
+    legend: {
+        position: 'right',
+        labels: {
+            boxWidth: 20,
+            fontstyle: 'bold'
+        }
+    }
+}
+
+//Creating Doughnut Chart
+let mobileChart = new Chart(mobileCanvas, {
+    type: 'doughnut',
+    data: mobileData,
+    options: mobileOptions
+});
+
+
+/*---Message Section---*/ 
+send.addEventListener('click', () => {
+    // ensure user and message fields are filled out
+    if (user.value === "" && message.value === "") {
+        alert("Please fill out user and message fields before sending");
+    } else if (user.value === "") {
+        alert("Please fill out user field before sending");
+    } else if (message.value === "") {
+        alert("Please fill out message field before sending");
+    } else {
+        alert(`Message successfully sent to: ${user.value}`);
+    }
+});
